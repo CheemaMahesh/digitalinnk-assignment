060969fa613e1eb37bdb78eb9be9336f32c31318
CheemaMahesh <maheshbabucheema789@gmail.com>
2026-03-31 14:45:52 +0530

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <section className="bg-red-500 w-screen h-screen">ada</section>;
+}
+
+export default App;
---
+++ src/utils/data/index.ts
@@ -0,0 +1,34 @@
+import type { Product } from "../../types";
+
+export const products: Product[] = [
+  {
+    id: 1,
+    name: "Bread",
+    price: 1.1,
+    currency: "£",
+  },
+  {
+    id: 2,
+    name: "Milk",
+    price: 0.5,
+    currency: "£",
+  },
+  {
+    id: 3,
+    name: "Cheese",
+    price: 0.9,
+    currency: "£",
+  },
+  {
+    id: 4,
+    name: "Soup",
+    price: 0.6,
+    currency: "£",
+  },
+  {
+    id: 5,
+    name: "Milk",
+    price: 1.2,
+    currency: "£",
+  },
+];
